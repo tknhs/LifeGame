4fec7e586560949ad5d873ced10ae166c36c9832
pobo380 <n3560k@gmail.com>
2013-12-14 17:06:43 +0900

    Defined interface of Field class.

---
+++ src/Field.java
@@ -0,0 +1,16 @@
+class Field {
+  public boolean cell_at(int x, int y) {
+    return null;
+  }
+
+  public int count_alive_near_by(point) {
+    return null;
+  }
+
+  public boolean next_cell_at(int x, int y) {
+    return null;
+  }
+
+  public void update() {
+  }
+}
