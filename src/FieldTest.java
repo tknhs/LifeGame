713cdd1f3f343cf59b5688e62291abc43d24ddb2
ono <yagfair@gmail.com>
2013-12-14 17:29:33 +0900

    updateのテスト追加

--- src/FieldTest.java
+++ src/FieldTest.java
@@ -34,4 +34,34 @@ public class FieldTest {
 		assertTrue(0==field.count_alive_near_by(10, 10));
 
 	}
+	
+	@Test
+	public void test3() {
+		int[][] points={
+				{1,0},
+				{1,1},
+				{1,2},
+		};
+		Field field=new Field(points);
+		assertTrue(field.next_cell_at(1, 1));
+		assertFalse(field.next_cell_at(1, 0));
+		assertFalse(field.next_cell_at(1, 2));
+
+	}
+	@Test
+	public void test4() {
+		int[][] points={
+				{1,0},
+				{1,1},
+				{1,2},
+		};
+		Field field=new Field(points);
+		field.update();
+		assertTrue(field.cell_at(0, 1));
+		assertTrue(field.cell_at(1, 1));
+		assertTrue(field.cell_at(2, 1));
+
+
+	}
+	
 }
