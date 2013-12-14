c91c17a36421c89724f1697c1fd1deed1b29e2bf
ono <yagfair@gmail.com>
2013-12-14 17:26:53 +0900

    count_alive_near_byのテスト追加

--- src/FieldTest.java
+++ src/FieldTest.java
@@ -13,7 +13,25 @@ public class FieldTest {
 				{1,2},
 		};
 		Field field=new Field(points);
+		assertTrue(field.cell_at(1, 0));
 		assertTrue(field.cell_at(1, 1));
+		assertTrue(field.cell_at(1, 2));
+
 	}
 
+	@Test
+	public void test2() {
+		int[][] points={
+				{1,0},
+				{1,1},
+				{1,2},
+		};
+		Field field=new Field(points);
+		assertTrue(2==field.count_alive_near_by(1, 1));
+		assertTrue(1==field.count_alive_near_by(1, 0));
+		assertTrue(1==field.count_alive_near_by(1, 2));
+		assertTrue(0==field.count_alive_near_by(5, 5));
+		assertTrue(0==field.count_alive_near_by(10, 10));
+
+	}
 }
