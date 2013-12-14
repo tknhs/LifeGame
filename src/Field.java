576ba0372674b1a0993f41447b9bfa694dad6585
pobo380 <n3560k@gmail.com>
2013-12-14 17:27:54 +0900

    cell_atまで実装した

--- src/Field.java
+++ src/Field.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 class Field {
+  public Set set;
+
+  public Field(int[][] points) {
+    this.set = new HashSet();
+
+    for (int i = 0; i < points.length; ++i) {
+      this.set.add(points[i]);
+    }
+  }
+
   public boolean cell_at(int x, int y) {
-    return null;
+    return this.set.contains({x, y});
   }
 
   public int count_alive_near_by(point) {
